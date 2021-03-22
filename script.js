17567bdc9abd727f2161da24eb51707acabc41ad
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-22 13:41:40 +0000

    Added comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// I'm setting these to 0 so they can be changed later.
 let b1 = 0
 let b2 = 0
 let height = 0
 let area = 0
 
+// I'm making it so it's looking for something with the id of button to be pushed which will cauese the function math to activate. 
 document.getElementById('button').addEventListener('click', math)
 
+// This funtion will preform the math.
 function math () {
   b1 = document.getElementById('input-1').value
   b1 = parseInt(b1)
